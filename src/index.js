b1b9587d647c075a59a0a84fe095383ba320a7bb
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-04 23:46:36 +0100

    stack navigation

--- src/index.js
+++ src/index.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  greetings: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.warn();
-('test');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.greetings}>Hello World!</Text>
-      <Text style={styles.greetings}>Hello World!</Text>
-    </View>
-  );
+  return <Routes />;
 }
