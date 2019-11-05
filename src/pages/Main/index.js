4f40abe0eef9b39740d46cfa0f28f6b3a9e3283a
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-05 22:47:06 +0100

    request users

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
 
 export default function Main() {
+  const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+
+  const handleSubmit = async () => {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  };
+
   return (
     <Container>
       <Form>
@@ -12,8 +32,12 @@ export default function Main() {
           autoCorrent={false}
           autoCapitalize="none"
           placeholder="Add user"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleSubmit}>
           <Icon name="add" size={20} color="#fff" />
         </SubmitButton>
       </Form>
