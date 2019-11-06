1d76ee51fa06bc03b34afed17cac9cae6f64dd69
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-06 23:54:41 +0100

    prop types plus navigation to user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function User() {
+const User = ({ navigation }) => {
+  console.tron.log(navigation.getParam('user'));
   return <View />;
-}
+};
+
+User.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
+
+export default User;
