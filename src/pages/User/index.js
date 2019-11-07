0e0b7f1a659ef68ade1cd50ba6c4ec8a601d6ce0
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-07 01:22:04 +0100

    logic and elements

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,23 +1,66 @@
 import React, { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 const User = ({ navigation }) => {
   const [stars, setStars] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const user = navigation.getParam('user');
+
   // didmount
-  useEffect(async () => {
-    const user = navigation.getParam('user');
+  useEffect(() => {
+    async function fetchStarred() {
+      setLoading(true);
+      const response = await api.get(`/users/${user.login}/starred`);
+      setStars(response.data);
+    }
+    fetchStarred();
+    setLoading(false);
+  }, [setStars, setLoading]);
 
-    const response = await api.get(`/users/${user.login}/starred`);
-
-    setStars(response.data);
-  }, [setStars]);
-
-  return <View />;
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{ uri: user.avatar }} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+      {loading ? (
+        <ActivityIndicator color="#000" />
+      ) : (
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      )}
+    </Container>
+  );
 };
 
 User.navigationOptions = ({ navigation }) => ({
