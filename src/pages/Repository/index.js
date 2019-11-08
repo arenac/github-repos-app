6203c657cf82500a54dc8e0333864a3fc7cb9b79
Nilo Neregato <nilo.neregato@gmail.com>
2019-11-08 23:54:49 +0100

    move title to navigationOptions

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,20 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WebView from 'react-native-webview';
 
-import { Container, Header, Name } from './styles';
-
 const Repository = ({ navigation }) => {
   const repository = navigation.getParam('repository');
 
-  return (
-    <Container>
-      <Header>
-        <Name>{repository.name}</Name>
-      </Header>
-      <WebView source={{ uri: repository.html_url }} style={{ flex: 1 }} />
-    </Container>
-  );
+  return <WebView source={{ uri: repository.html_url }} style={{ flex: 1 }} />;
 };
+
+Repository.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('repository').name,
+});
 
 Repository.propTypes = {
   navigation: PropTypes.shape({
